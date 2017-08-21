40c6da19ba06739ba3516beec60893ff6cf5a07e
Madhur Taneja <tanejamadhur96@gmail.com>
2017-08-21 16:20:11 +0530

    makeGrid function completed

--- designs.js
+++ designs.js
@@ -3,11 +3,25 @@
 
 // When size is submitted by the user, call makeGrid()
 
+var table = $('#pixel_canvas');
+var columns;
+var rows;
+
 function makeGrid(rows, columns) {
 	
 // Your code goes here!
-
-
-
+	var grid = '';
+	for (var i = 0; i < rows; i++) {
+		grid += `<tr>`;
+		for (var j = 0; j < columns; j++) {
+			grid += `<td class="cell">&nbsp;</td>`;
+		}
+			grid += `</tr>`;
+	}
+  
+	table.html(grid);
 
 }
+
+// Build a default 10x10 grid.
+makeGrid(10,10);
